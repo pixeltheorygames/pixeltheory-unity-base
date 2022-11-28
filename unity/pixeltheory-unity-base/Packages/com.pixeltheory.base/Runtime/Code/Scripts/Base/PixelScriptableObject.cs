f2eb5751866e791867698753232df6336af32fac
Ellis Talley <git@ellistalley.com>
2022-11-28 16:16:32 +0900

    Adding Unity project to repo.

---
+++ unity/pixeltheory-unity-base/Packages/com.pixeltheory.base/Runtime/Code/Scripts/Base/PixelScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Pixeltheory
+{
+    [Serializable]
+    public class PixelScriptableObject : ScriptableObject 
+    {
+        /*
+         * Reserved for future use.
+         *  - Ellis 2022.11.25
+         */
+    }   
+}
