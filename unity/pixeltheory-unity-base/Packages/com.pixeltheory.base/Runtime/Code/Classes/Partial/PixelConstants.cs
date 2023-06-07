9b7fa0b563c4f5cf332156ad373908b69b09d54c
Ellis Talley <git@ellistalley.com>
2023-06-07 12:43:21 +0900

    Added PixelConstants.

---
+++ unity/pixeltheory-unity-base/Packages/com.pixeltheory.base/Runtime/Code/Classes/Partial/PixelConstants.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Pixeltheory
+{
+    public static partial class PixelConstants
+    {
+        /*
+         *  Partial class other Pixeltheory packages can extend
+         *  and add their needed static constants.
+         */
+    }
+}
