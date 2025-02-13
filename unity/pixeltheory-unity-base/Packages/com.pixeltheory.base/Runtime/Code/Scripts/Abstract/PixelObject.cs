03edb938bb9545a1c6a8ba4980a21bbd2fa789fe
Ellis Talley <ellis@pixeltheorygames.com>
2025-02-13 15:23:56 -0500

    Added object state to help with OnEnable/OnDisable.

--- unity/pixeltheory-unity-base/Packages/com.pixeltheory.base/Runtime/Code/Scripts/Abstract/PixelObject.cs
+++ unity/pixeltheory-unity-base/Packages/com.pixeltheory.base/Runtime/Code/Scripts/Abstract/PixelObject.cs
@@ -7,6 +7,15 @@ namespace Pixeltheory
     [Serializable]
     public abstract class PixelObject : ScriptableObject 
     {
-        
+        #region Fields
+        #region Protected
+        protected enum PixelObjectState
+        {
+            Disabled,
+            Enabled
+        }
+        protected PixelObjectState currentPixelObjectState;
+        #endregion //Proteced
+        #endregion //Fields
     }   
 }
